5956f954b6f810424220a4a1e7b992ffb56fd9b7
lekhanhduylam <lekhanhduylam@gmail.com>
2022-01-26 11:06:34 +0700

    add sql file

---
+++ src/main/resources/SQLQueryUserManagement.sql
@@ -0,0 +1,8 @@
+DROP DATABASE StudentManagermentDatabase;
+CREATE DATABASE StudentManagermentDatabase;
+USE StudentManagermentDatabase;
+CREATE TABLE Users (
+	Id int,
+	UserName varchar(20),
+	PassWords varchar(20) 
+)
